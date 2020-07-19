e5982551850eabcad1871e82eca83ac43d0502be
harajune <harajune@gijutsuya.jp>
2020-07-19 20:04:03 +0900

    Initial Commit

---
+++ WebViewTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebViewTest
+//
+//  Created by Jun Harada on 2020/07/19.
+//  Copyright © 2020 Jun Harada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
